70b52adae73139d49e230ed722bd14f6feb2e271
Alejandratala <70092696+Alejandratala@users.noreply.github.com>
2021-03-18 12:32:29 +0100

    Starting the classes and Project

---
+++ Source/SpaceParkModel/Data/Occupancy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceParkModel.Data
+{
+    class Occupancy
+    {
+        public int ID { get; set; }
+        public int PersonID { get; set; }
+        public int SpaceshipID { get; set; }
+        // if we arent putting Payment ...
+        public bool HasPaid { get; set; }
+        public DateTime ArrivalTime { get; set; }
+        // are we leaving the entry in the database? or leave it and mark it as finished
+        public bool HasLeft { get; set; }
+    }
+}
---
+++ Source/SpaceParkModel/Data/PriceBreakPoint.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceParkModel.Data
+{
+    class PriceBreakPoint
+    {
+        // set the hours as ID, since they have to be unique
+        public int Hours { get; set; }
+        public decimal Price { get; set; }
+    }
+}
