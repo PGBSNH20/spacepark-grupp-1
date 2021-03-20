06db729ea4e613b9a7dad383bda46d75f8268775
Patric Bergkvist <70053524+Spuute@users.noreply.github.com>
2021-03-20 19:09:59 +0100

    Added classes for spacepark and parkingspot

--- Source/SpaceEngine/Program.cs
+++ Source/SpaceEngine/Program.cs
@@ -1,6 +1,7 @@
 ﻿using RestSharp;
 using System;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace SpaceEngine
 {
@@ -8,26 +9,61 @@ namespace SpaceEngine
     {
         public static async Task Main(string[] args)
         {
-            bool menuGoing = true;
-            Character character = await SpaceOrm.CharacterSelection();
-            Starship starship = await SpaceOrm.GetStarShips(character);
+            List<string> test = new List<string>();
+            test.Add("https://swapi.dev/api/starships/16/");
 
-            while (menuGoing)
+            var vader = SpaceOrm.ValidateStarship(test);
+
+            foreach (var a in vader)
             {
-                int selectedOption = Menu.ShowMenu($"What do you want to do {character.Name}?\n", new[]
-                {
-                    "Park Vehicle",
-                    "Unpark Vehicle",
-                    "Show parking history",
-                     "Exit"
-                });
-
-                if(selectedOption == 3)
-                {
-                    menuGoing = false;
-                    Console.WriteLine("Have a nice day!");
-                }
+                Console.WriteLine(a.re);
             }
+
+            Spacepark first = new Spacepark()
+            {
+                Name = "First"
+            };
+
+            Parkingspot.Small hej = new Parkingspot.Small()
+            {
+                Arrival = DateTime.Now,
+                Departure = DateTime.Now.AddHours(1),
+                Starship = vader.Result[0]
+            };
+
+            Console.WriteLine(hej.Starship.Name);
+
+
+
+
+
+
+
+
+
+
+            //bool menuGoing = true;
+            //Character character = await SpaceOrm.CharacterSelection();
+            //Starship starship = await SpaceOrm.GetStarShips(character);
+
+            //while (menuGoing)
+            //{
+            //    int selectedOption = Menu.ShowMenu($"What do you want to do {character.Name}?\n", new[]
+            //    {
+            //        "Park Vehicle",
+            //        "Unpark Vehicle",
+            //        "Show parking history",
+            //         "Exit"
+            //    });
+
+            //    if(selectedOption == 3)
+            //    {
+            //        menuGoing = false;
+            //        Console.WriteLine("Have a nice day!");
+            //    }
+            //}
+
+
         }
     }
 }
---
+++ Source/SpaceEngine/Spacepark.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceEngine
+{
+    public class Spacepark
+    {
+        public string Name { get; set; }
+        public List<Parkingspot> Parkings { get; set; }
+    }
+}
