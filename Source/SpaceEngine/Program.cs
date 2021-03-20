2b45a907d66e81326d6d5f190aa4d58f6393c415
Patric Bergkvist <70053524+Spuute@users.noreply.github.com>
2021-03-20 12:02:56 +0100

    Added functionallity to handle list of people and menu for choices  after choosing character and ship

--- Source/SpaceEngine/Program.cs
+++ Source/SpaceEngine/Program.cs
@@ -8,9 +8,31 @@ namespace SpaceEngine
     {
         public static async Task Main(string[] args)
         {
+            Console.WriteLine("Hello Traveler, Welcome to SpacePark!\n");
             Console.Write("Enter your name: ");
             string input = Console.ReadLine();
             await SpaceOrm.ValidateCharacter(input);
+
+            bool menuGoing = true;
+            Console.WriteLine("Welcome!");
+            Console.WriteLine();
+
+            while (menuGoing)
+            {
+                int selectedOption = Menu.ShowMenu("What do you want to do?\n", new[]
+                {
+                    "Park Vehicle",
+                    "Unpark Vehicle",
+                    "Show parking history",
+                     "Exit"
+                });
+
+                if(selectedOption == 3)
+                {
+                    menuGoing = false;
+                    Console.WriteLine("Have a nice day!");
+                }
+            }
         }
     }
 }
