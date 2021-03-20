43d7326616823c2155f5d9b818444366f27d50a6
Patric Bergkvist <70053524+Spuute@users.noreply.github.com>
2021-03-20 12:56:11 +0100

    Missed file from last commit

--- Source/SpaceEngine/SpaceOrm.cs
+++ Source/SpaceEngine/SpaceOrm.cs
@@ -10,27 +10,24 @@ namespace SpaceEngine
 {
     public class SpaceOrm
     {
+        // Using the API search function to get the Character
         public static async Task ValidateCharacter(string input)
         {
             var client = new RestClient("https://swapi.dev/api/");
             var request = new RestRequest($"people/?search={input}", DataFormat.Json);
             var peopleResponse = await client.GetAsync<PeopleReponse>(request);
 
-            if (peopleResponse.Results.Count != 1)
-            {
-                Console.WriteLine("Invalid character");
-            }
-            else
-            {
-                Console.WriteLine($"Welcome {peopleResponse.Results[0].Name}");
-                //Console.WriteLine(peopleResponse.Results[0].ShipUrls);
-                await GetStarShips(peopleResponse.Results[0].StarShips);
-            }
+            string[] characters = peopleResponse.Results.Select(x => x.Name).ToArray();
+            int selection = Menu.ShowMenu("Who are you? ", characters);
+            Console.Clear();
+            Console.WriteLine($"\nWelcome {peopleResponse.Results[selection].Name}");
+            await GetStarShips(peopleResponse.Results[0].StarShips);
         }
 
         public static async Task GetStarShips(List<string> starshipUrls)
         {
             List<Starship> ships = new();
+
             foreach (var url in starshipUrls)
             {
                 var client = new RestClient(url);
@@ -38,17 +35,19 @@ namespace SpaceEngine
                 var response = await client.GetAsync<Starship>(request);
                 ships.Add(response);
             }
+
             if (ships.Count != 0)
             {
                 string[] shipOptions = ships.Select(s => s.Name).ToArray();
                 int selection = Menu.ShowMenu("\nChoose your ship", shipOptions);
 
-                Console.WriteLine("You selected" + " " + ships[selection].Name);
+                Console.WriteLine("\nYou selected" + " " + ships[selection].Name);
             }
             else
             {
                 Console.WriteLine("You have no ships");
             }
+            Console.Clear();
         }
     }
 }
