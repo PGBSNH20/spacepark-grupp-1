920abcace70476c488bc9c0a9d7ff28260859f19
ItsAnass <domanee@hotmail.com>
2021-03-24 10:55:10 +0100

    Fixed the menu and added some coola funktioner :)

--- Source/SpaceEngine/ORM/SpaceORM.cs
+++ Source/SpaceEngine/ORM/SpaceORM.cs
@@ -10,55 +10,7 @@ namespace SpaceEngine
 {
     public class SpaceORM
     {
-        // Using the API search function to get the Character
-        public static async Task<Character> CharacterSelection()
-        {
-            bool characterSelection = true;
-            PeopleReponse peopleResponse = new();
-            int selection = -1;
-            while (characterSelection)
-            {
-                Console.WriteLine("Hello Traveler, Welcome to SpacePark!\n");
-                Console.Write("Enter your name: ");
-                string input = Console.ReadLine();
-                peopleResponse = await ValidateCharacter(input);
-                if (peopleResponse.Results.Count != 0)
-                {
-                    string[] characters = peopleResponse.Results.Select(x => x.Name).ToArray();
-                    selection = Menu.ShowMenu("\nWho are you?", characters);
-                    Console.Clear();
-                    Console.WriteLine($"\nWelcome {peopleResponse.Results[selection].Name}");
-                    characterSelection = false;
-                }
-                else
-                {
-                    int _continue = Menu.ShowMenu("Do you wish to try again?", new[] { "Yes", "No" });
-                    if (_continue == 1)
-                    {
-                        Environment.Exit(0);
-                    }
-                }
-            }
-            return peopleResponse.Results[selection];
-        }
-        public static async Task<Starship> GetStarShips(Character character)
-        {
-            List<Starship> ships = await ValidateStarship(character.StarShips);
-            int selection = -1;
-            if (ships.Count != 0)
-            {
-                string[] shipOptions = ships.Select(s => s.Name).ToArray();
-                selection = Menu.ShowMenu("\nChoose your ship", shipOptions);
-                Console.WriteLine("\nYou selected" + " " + ships[selection].Name);
-            }
-            else
-            {
-                Console.WriteLine("\nYou have no ships");
-                Environment.Exit(0);
-            }
-            Console.Clear();
-            return ships[selection];
-        }
+        // Using the API search function to get the Character       
         public static async Task<PeopleReponse> ValidateCharacter(string input)
         {
             var client = new RestClient("https://swapi.dev/api/");
--- Source/SpaceEngine/Program.cs
+++ Source/SpaceEngine/Program.cs
@@ -12,46 +12,19 @@ namespace SpaceEngine
     {
         public static async Task Main(string[] args)
         {
-            CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
-            bool menuGoing = true;
-
-            Print.AvailableSpots();
-
-            Character character = await SpaceORM.CharacterSelection();
-            Starship starship = await SpaceORM.GetStarShips(character);
-            while (menuGoing)
-            {
-                int selectedOption = Menu.ShowMenu($"What do you want to do {character.Name}?\n", new[]
-                {
-                    "Park Vehicle",
-                    "Unpark Vehicle",
-                    "Show parking history",
-                     "Exit"
-                });
-                if (selectedOption == 0)
-                {
-                    Parkingspot.Park(starship, character);
-                }
-                else if (selectedOption == 1)
-                {
-                    Parkingspot.Unpark(starship, character);
-                }
-                else if (selectedOption == 2)
-                {
-
-                }
-                else 
-                {
-                    menuGoing = false;
-                    Console.WriteLine("Have a nice day!");
-                }
-            }
+            Print.TakenSpots();
+            CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;          
+            Character character = await Menu.CharacterSelection();
+            Starship starship = await Menu.GetStarShips(character);
+            Menu.Start(character, starship);
+                       
         }
     }
 }
 // TODO: Separate menu from SpaceORM
 // TODO: Close DB Connections
 // TODO: Add try again option for entering character name
+// TODO: Fix the menu
 // TODO: More unit tests
 // TODO: Add more todo's where you see fit
 
