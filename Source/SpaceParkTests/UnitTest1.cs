4e7a9a2f0585badca7a250f575e840c5468cc3fd
Jonas Lindell <70151761+jonaslindell-th@users.noreply.github.com>
2021-03-28 23:29:18 +0200

    Update UnitTest1.cs

--- Source/SpaceParkTests/UnitTest1.cs
+++ Source/SpaceParkTests/UnitTest1.cs
@@ -14,8 +14,8 @@ namespace SpaceParkTests
         public void ValidateCharacter_CorrectInput_ExpectTrue(string input)
         {
             var character = API.ValidateCharacter(input);
-            bool isTraveler = character.Result.Results[0].Name == input;
-            Assert.True(isTraveler);
+            bool correctCharacterName = character.Result.Results[0].Name == input;
+            Assert.True(correctCharacterName);
         }
 
         [Theory]
@@ -25,21 +25,36 @@ namespace SpaceParkTests
         public void ValidateCharacter_IncorrectInput_ExpectFalse(string input)
         {
             var character = API.ValidateCharacter(input);
-            bool isTraveler = character.Result.Results.Count == 1;
-            Assert.False(isTraveler);
+            bool containsCharacter = character.Result.Results.Count == 1;
+            Assert.False(containsCharacter);
         }
 
         [Theory]
-        [InlineData("https://swapi.dev/api/starships/12/", "https://swapi.dev/api/starships/16/")]
-        public void ValidateStarship_CorrectInpute_ExpectTrue(string input, string input2)
+        [InlineData("https://swapi.dev/api/starships/12/", "X-wing")]
+        [InlineData("https://swapi.dev/api/starships/17/", "Rebel transport")]
+        [InlineData("https://swapi.dev/api/starships/13/", "TIE Advanced x1")]
+        public void ValidateStarshipName_CorrectInpute_ExpectTrue(string input, string expected)
         {
             List<string> ships = new();
             ships.Add(input);
-            ships.Add(input2);
 
             var ship = API.ValidateStarship(ships);
-            bool isShip = ship.Result.Count == 2;
-            Assert.True(isShip);
+            bool correctShipName = ship.Result[0].Name == expected;
+            Assert.True(correctShipName);
+        }
+
+        [Theory]
+        [InlineData("https://swapi.dev/api/starships/12/", "T-Wing")]
+        [InlineData("https://swapi.dev/api/starships/17/", "QN48b")]
+        [InlineData("https://swapi.dev/api/starships/13/", "XC 70")]
+        public void ValidateStarshipName_CorrectInpute_ExpectFalse(string input, string expected)
+        {
+            List<string> ships = new();
+            ships.Add(input);
+
+            var ship = API.ValidateStarship(ships);
+            bool correctShipName = ship.Result[0].Name == expected;
+            Assert.False(correctShipName);
         }
     }
 }
