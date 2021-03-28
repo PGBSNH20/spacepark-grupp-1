28a6629eb91efa2967223a8cd41976bfc48e4060
Patric Bergkvist <70053524+Spuute@users.noreply.github.com>
2021-03-28 23:32:30 +0200

    Renamed tests

--- Source/SpaceParkTests/UnitTest1.cs
+++ Source/SpaceParkTests/UnitTest1.cs
@@ -11,7 +11,7 @@ namespace SpaceParkTests
         [InlineData("Luke Skywalker")]
         [InlineData("Leia Organa")]
         [InlineData("Darth Vader")]
-        public void ValidateCharacter_CorrectInput_ExpectTrue(string input)
+        public void ValidateCharacterName_CorrectInput_ExpectTrue(string input)
         {
             var character = API.ValidateCharacter(input);
             bool correctCharacterName = character.Result.Results[0].Name == input;
@@ -22,7 +22,7 @@ namespace SpaceParkTests
         [InlineData("Jonas Schmedtmann")]
         [InlineData("Spacejam george")]
         [InlineData("Darth Peter")]
-        public void ValidateCharacter_IncorrectInput_ExpectFalse(string input)
+        public void ValidateCharacterName_IncorrectInput_ExpectFalse(string input)
         {
             var character = API.ValidateCharacter(input);
             bool containsCharacter = character.Result.Results.Count == 1;
@@ -33,7 +33,7 @@ namespace SpaceParkTests
         [InlineData("https://swapi.dev/api/starships/12/", "X-wing")]
         [InlineData("https://swapi.dev/api/starships/17/", "Rebel transport")]
         [InlineData("https://swapi.dev/api/starships/13/", "TIE Advanced x1")]
-        public void ValidateStarshipName_CorrectInpute_ExpectTrue(string input, string expected)
+        public void ValidateStarshipName_CorrectInput_ExpectTrue(string input, string expected)
         {
             List<string> ships = new();
             ships.Add(input);
@@ -47,7 +47,7 @@ namespace SpaceParkTests
         [InlineData("https://swapi.dev/api/starships/12/", "T-Wing")]
         [InlineData("https://swapi.dev/api/starships/17/", "QN48b")]
         [InlineData("https://swapi.dev/api/starships/13/", "XC 70")]
-        public void ValidateStarshipName_CorrectInpute_ExpectFalse(string input, string expected)
+        public void ValidateStarshipName_IncorrectInput_ExpectFalse(string input, string expected)
         {
             List<string> ships = new();
             ships.Add(input);
